f60cfd075a3c5fe05d4ed71a6ae862dba1729ac1
Shiori-Takanashi <nao69alter@gmail.com>
2025-05-06 20:15:31 +0900

    onetable splited?

--- src/components/SequenceTable.jsx
+++ src/components/SequenceTable.jsx
@@ -9,74 +9,27 @@ export default function SequenceTable({ sequence }) {
 
     return (
         <Box bg="gray.100" p={2} borderWidth="1px" borderColor="gray.200">
-            <Table size="xs" borderCollapse="collapse" marginBottom={1}>
-                <Tbody>
-                    <Tr>
-                        {sequence.slice(0, rowCount_top + 1).map((seq, idx) => (
-                            <React.Fragment key={`header-${idx}`}>
-                                <Th textAlign="center" textColor="red.500" bg="gray.50" border="1px" borderColor="gray.200" fontSize="smaller">
-                                    {seq.button}
-                                </Th>
-                                <Th textAlign="center" textColor="gray.500" bg="gray.100" border="1px" borderColor="gray.200" fontSize="smaller">
-                                    待機
-                                </Th>
-                            </React.Fragment>
-                        ))}
-                        <Th textAlign="center" textColor="red.500" border="1px" borderColor="gray.200" fontSize="smaller">
-                            {sequence[len - 1].button}
-                        </Th>
-                    </Tr>
-                    <Tr>
-                        {sequence.slice(0, rowCount_top + 1).map((seq, idx) => (
-                            <React.Fragment key={`data-${idx}`}>
-                                <Td textAlign="center" textColor="blue.500" bg="gray.50" border="1px" borderColor="gray.200" fontSize="smaller">
-                                    {seq.press}
-                                </Td>
-                                <Td textAlign="center" textColor="green.500" bg="gray.100" border="1px" borderColor="gray.200" fontSize="smaller">
-                                    {seq.pause}
-                                </Td>
-                            </React.Fragment>
-                        ))}
-                        <Td textAlign="center" textColor="blue.500" border="1px" borderColor="gray.200" fontSize="smaller">
-                            {sequence[len - 1].press}
-                        </Td>
-                    </Tr>
-                </Tbody>
-            </Table>
             <Table size="xs" borderCollapse="collapse">
                 <Tbody>
                     <Tr>
-                        {sequence.slice(rowCount_top, rowCount_bottom + 1).map((seq, idx) => (
+                        {sequence.map((seq, idx) => (
                             <React.Fragment key={`header-${idx}`}>
-                                <Th textAlign="center" textColor="red.500" bg="gray.50" border="1px" borderColor="gray.200" fontSize="smaller">
-                                    {seq.button}
-                                </Th>
-                                <Th textAlign="center" textColor="gray.500" bg="gray.100" border="1px" borderColor="gray.200" fontSize="smaller">
-                                    待機
-                                </Th>
+                                <Th width="80px" textAlign="center" verticalAlign="middle">…</Th>
+                                <Th width="80px" textAlign="center" verticalAlign="middle">待機</Th>
                             </React.Fragment>
                         ))}
-                        <Th textAlign="center" textColor="red.500" border="1px" borderColor="gray.200" fontSize="smaller">
-                            {sequence[len - 1].button}
-                        </Th>
                     </Tr>
                     <Tr>
-                        {sequence.slice(rowCount_top, rowCount_bottom + 1).map((seq, idx) => (
+                        {sequence.map((seq, idx) => (
                             <React.Fragment key={`data-${idx}`}>
-                                <Td textAlign="center" textColor="blue.500" bg="gray.50" border="1px" borderColor="gray.200" fontSize="smaller">
-                                    {seq.press}
-                                </Td>
-                                <Td textAlign="center" textColor="green.500" bg="gray.100" border="1px" borderColor="gray.200" fontSize="smaller">
-                                    {seq.pause}
-                                </Td>
+                                <Td width="80px" textAlign="center" verticalAlign="middle">…</Td>
+                                <Td width="80px" textAlign="center" verticalAlign="middle">…</Td>
                             </React.Fragment>
                         ))}
-                        <Td textAlign="center" textColor="blue.500" border="1px" borderColor="gray.200" fontSize="smaller">
-                            {sequence[len - 1].press}
-                        </Td>
                     </Tr>
                 </Tbody>
             </Table>
+
         </Box>
     );
 }
